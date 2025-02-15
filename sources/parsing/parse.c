b6049db573d918a84550e00d83bbd0a3d1e7d23d
Vitali Kostandyan <v.kostandyan05@gmail.com>
2025-02-15 16:08:25 +0400

    Fixed norminette: parse.c

--- sources/parsing/parse.c
+++ sources/parsing/parse.c
@@ -3,80 +3,83 @@
 /*                                                        :::      ::::::::   */
 /*   parse.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgalstya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vkostand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 14:46:33 by vkostand          #+#    #+#             */
-/*   Updated: 2025/02/10 19:35:06 by kgalstya         ###   ########.fr       */
+/*   Updated: 2025/02/15 16:07:53 by vkostand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parsing.h"
 
-// static void check_douplicate_textures()
-// {
-
-// }
-
-static void read_textures(t_parse *data, int fd)
+static void	check_and_save_textures(char *str, t_parse *data, int fd)
 {
-    int not_empty_line;
-    char *str;
-    int type;
-    int status = 0;
+	int	type;
+	int	status;
 
-    not_empty_line = 0;
-    while(not_empty_line < 6)
-    {
-        str = get_next_line(fd);
-        if(!str)
-            break ;
-        if(is_empty_line(str, " \n\t\v"))
-        {
-            free_and_set_null(str);
-            continue;
-        }
-        type = check_type(str, data, fd, type, status);
-        status = save_textures(data, str, type);
-        if(status != SUCCESS)
-        {
-            decide_error(str, type, status);
-            free_and_set_null(str);
-            close(fd);
-            clean_parsing_data(data);
-            // system("leaks cub3D");
-            exit(1);
-        }
-        free_and_set_null(str);
-        not_empty_line++;
-    }
-    // if(not_empty_line != 6)
-    //     send_error("\033[1;31mError\033[0m\nNot enough textures\n");
+	type = check_type(str);
+	status = 0;
+	if (type == NOT_VALID)
+	{
+		decide_error(str, type, status);
+		clean_parsing_data(data);
+		close(fd);
+		free_and_set_null(str);
+		system("leaks cub3D");
+		exit(1);
+	}
+	status = save_textures(data, str, type);
+	if (status != SUCCESS)
+	{
+		decide_error(str, type, status);
+		free_and_set_null(str);
+		close(fd);
+		clean_parsing_data(data);
+		system("leaks cub3D");
+		exit(1);
+	}
 }
 
-void fill_parse_data_with_null(t_parse *data)
+static void	read_textures(t_parse *data, int fd)
 {
-    data->ceiling_color = 0;
-    data->floor_color = 0;
+	int		not_empty_line;
+	char	*str;
+
+	not_empty_line = 0;
+	while (not_empty_line < 6)
+	{
+		str = get_next_line(fd);
+		if (!str)
+			break ;
+		if (is_empty_line(str, " \n\t\v"))
+		{
+			free_and_set_null(str);
+			continue ;
+		}
+		check_and_save_textures(str, data, fd);
+		free_and_set_null(str);
+		not_empty_line++;
+	}
+	if (not_empty_line != 6)
+		send_error("Not enough textures\n");
 }
 
-t_parse parse(int argc, char **argv)
+t_parse	parse(int argc, char **argv)
 {
-    t_parse data;
-    int fd;
+	t_parse	data;
+	int		fd;
 
-    ft_bzero(&data, sizeof(data));
-    check_name(argc, argv);
-    fd = open(argv[1], O_RDONLY);
-    if (fd == -1)
+	ft_bzero(&data, sizeof(data));
+	check_name(argc, argv);
+	fd = open(argv[1], O_RDONLY);
+	if (fd == -1)
 		send_error(FILE_OPEN_ERR);
-    read_textures(&data, fd);
-    read_and_parse_map(&data, fd);
-    get_player_position(&data);
-    get_map_height_and_width(&data);
-    // get_textures_fds(&data);
-    return (data);
-    clean_parsing_data(&data);
-    (void)argc;
-    (void)argv;
-    (void)data;
+	read_textures(&data, fd);
+	read_and_parse_map(&data, fd);
+	get_player_position(&data);
+	get_map_height_and_width(&data);
+	return (data);
+	(void)argc;
+	(void)argv;
+	(void)data;
 }
