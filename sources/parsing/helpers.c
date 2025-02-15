c6c0676eba86a6e1b407d0122487924a0c4d70ee
Vitali Kostandyan <v.kostandyan05@gmail.com>
2025-02-15 16:20:50 +0400

    Fixed norminette: helpers.c

--- sources/parsing/helpers.c
+++ sources/parsing/helpers.c
@@ -3,14 +3,12 @@
 /*                                                        :::      ::::::::   */
 /*   helpers.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kgalstya <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: vkostand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/25 17:29:56 by vkostand          #+#    #+#             */
-/*   Updated: 2025/02/10 17:56:10 by kgalstya         ###   ########.fr       */
+/*   Updated: 2025/02/15 16:19:58 by vkostand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// 3 functions
 
 #include "parsing.h"
 
@@ -26,7 +24,7 @@ int	is_empty_line(char *str, char *set)
 	return (1);
 }
 
-t_type	check_type(char *str, t_parse *data, int fd, t_type type, int status)
+t_type	check_type(char *str)
 {
 	int	i;
 
@@ -47,70 +45,34 @@ t_type	check_type(char *str, t_parse *data, int fd, t_type type, int status)
 		return (FLOOR);
 	if (ft_strstr(str + i, "C"))
 		return (CEILING);
-	printf("blabla\n");
-	decide_error(str, type, status);
-	clean_parsing_data(data);
-	close(fd);
-	free_and_set_null(str);
-    // system("leaks cub3D");
-	exit(1);
 	return (NOT_VALID);
 }
 
-void free_and_set_null(char *str)
+void	free_and_set_null(char *str)
 {
-	if(str)
+	if (str)
 	{
 		free(str);
 		str = NULL;
 	}
 }
 
-void clean_parsing_data(t_parse *data)
+void	clean_parsing_data(t_parse *data)
 {
-    free_array(data->map);
+	free_array(data->map);
 	data->map = NULL;
-    if(data->north)
+	if (data->north)
 		free_and_set_null(data->north);
-    if(data->south)
+	if (data->south)
 		free_and_set_null(data->south);
-    if(data->east)
+	if (data->east)
 		free_and_set_null(data->east);
-    if(data->west)
+	if (data->west)
 		free_and_set_null(data->west);
 	close(data->east_fd);
 	close(data->south_fd);
 	close(data->north_fd);
 	close(data->west_fd);
-}
-
-char	*ft_join(char const *s1, char const *s2)
-{
-	char	*s;
-	int		i;
-	int		j;
-
-	if (!s2)
-		return (NULL);
-	s = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 2));
-	if (s == NULL)
-		return (NULL);
-	i = 0;
-	j = 0;
-	while (s1[i] != '\0')
-	{
-		s[i] = s1[i];
-		i++;
-	}
-	if (i != 0)
-		s[i++] = '\n';
-	while (s2[j] != '\0')
-	{
-		s[i + j] = s2[j];
-		j++;
-	}
-	s[i + j] = '\0';
-	return (s);
 }
 
 void	check_name(int argc, char **argv)
